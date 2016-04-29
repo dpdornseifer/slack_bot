db3788e64ccbc36542eab7e6bcc4e0d0a9d6b972
david <david-paul.dornseifer@sap.com>
2016-04-28 17:03:56 -0700

    Added settings file

---
+++ slackbot_settings.py
@@ -0,0 +1,15 @@
+API_TOKEN = ""
+
+PLUGINS = [
+        'plugins'
+]
+
+TEAM_NAME = 'Your dev team
+
+CAFES = {
+        'cafe1': 246,
+        'cafe3': 245,
+        'cafe8': 247
+}
+
+CAFES_DEFAULT = 246
